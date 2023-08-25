cc320f87cf832018a6210be1961a923a9b6e78f0
amirs18 <amir135246@gmail.com>
2023-08-25 03:38:35 +0300

    working migrations working

--- src/db/database.ts
+++ src/db/database.ts
@@ -7,9 +7,7 @@ import {
   MigrationProvider,
   FileMigrationProvider,
 } from "kysely";
-import * as path from "path";
-import { promises as fs } from "fs";
-import {up} from "../db/migrations/migrations"
+import path from "path";
 
 const dialect = new PostgresDialect({
   pool: new Pool({
@@ -29,38 +27,8 @@ const dialect = new PostgresDialect({
 export const db = new Kysely<Database>({
   dialect,
 });
-// up(db)
-// async function migrateToLatest() {
 
 
-//   const migrator = new Migrator({
-//     db,
-//     provider: new FileMigrationProvider({
-//       fs,
-//       path,
-//       // This needs to be an absolute path.
-//       migrationFolder: path.join(__dirname, '../db/migrations'),
-//     }),
-//   })
 
-//   const { error, results } = await migrator.migrateToLatest()
 
-//   results?.forEach((it) => {
-//     if (it.status === 'Success') {
-//       console.log(`migration "${it.migrationName}" was executed successfully`)
-//     } else if (it.status === 'Error') {
-//       console.error(`failed to execute migration "${it.migrationName}"`)
-//     }
-//   })
-
-//   if (error) {
-//     console.error('failed to migrate')
-//     console.error(error)
-//     process.exit(1)
-//   }
-
-//   await db.destroy()
-// }
-
-// migrateToLatest()
 
