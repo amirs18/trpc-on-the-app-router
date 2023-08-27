0491d5de46a1b0457c160988ccb9428ee0a3202b
amirs18 <amir135246@gmail.com>
2023-08-28 02:17:13 +0300

    added dynamyc to route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { serverClient } from "./_trpc/serverClient";
 
 import TodoList from "./_components/TodoList";
+export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   const todos = await serverClient.getTodos();
